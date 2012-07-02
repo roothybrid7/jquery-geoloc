490544c5937e9d3ee5b45ea3952615caf6e78e57
Satoshi Ohki <roothybrid7@gmail.com>
2012-07-02 18:55:52 +0900

    Implements getcurrentposition and watchposition.

---
+++ dist/jquery-geoloc.js
@@ -0,0 +1,104 @@
+/*! jquery-geoloc - v0.0.0 - 2012-07-02
+* Copyright (c) 2012 Satoshi Ohki; Licensed MIT, GPLv2 */
+
+/*!
+ * jquery-geoloc.js - jQuery plugin for geolocation.
+ */
+
+;(function($, global, undefined) {
+  'use strict';
+
+  var pluginName = 'geoloc',
+      version = '0.0.0';
+
+  var defaults = {};
+
+  /**
+   * Plugin constructor.
+   * @param {Object} options A plugin options.
+   * @constructor
+   */
+  function Plugin(options) {
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this._clearWatchId = null;
+    this.init(options);
+  }
+
+  Plugin.prototype = {
+    init: function(options) {
+      this.watchPosition(options);
+    },
+    watchPosition: function(options) {
+      this.clearWatch();
+      this._clearWatchId = navigator.geolocation.watchPosition(
+        this.onSuccessWatchPosition, this.onErrorWatchPosition, options);
+    },
+    clearWatch: function() {
+      var id = this._clearWatchId;
+      id && navigator.geolocation.clearWatch(id);
+      this._clearWatchId = null;
+    },
+    /**
+     * On success callback with notifying.
+     *    trigger: success
+     * @param {Position} position The container for the geolocation information.
+     */
+    onSuccessWatchPosition: function(position) {
+      var event = jQuery.Event('success');
+      event.position = position;  // {coords: Coordinates, timestamp: [timestamp]}
+      $(navigator.geolocation).trigger(event, position);
+    },
+    /**
+     * On Error callback with notifying.
+     *    trigger: denied, unavailable, timeout, unknownerror.
+     * @param {PositionError} error The positionError container.
+     */
+    onErrorWatchPosition: function(error) {
+      this.clearWatchPosition();  // Clear curernt watchPosition.
+      var eventType = null;
+      switch (error.code) {
+        case error.PERMISION_DENIED:
+          eventType = 'denied';
+          break;
+        case error.POSITION_UNAVAILABLE:
+          eventType = 'unavailable';
+          break;
+        case error.TIMEOUT:
+          eventType = 'timeout';
+          break;
+        default:
+          eventType = 'unknownerror';
+          break;
+      }
+      var event = jQuery.Event(eventType);
+      event.error = error;  // {code: [1-3], message: 'An error message'}
+      $(navigator.geolocation).trigger(event, error);
+    },
+  };
+
+  /**
+   * Geolocation api with jQuery wrappers.
+   * @see http://dev.w3.org/geo/api/spec-source.html
+   */
+  $[pluginName] = {
+    /**
+     * @param {PositionOptions} options HTML5 Geolocation properties.
+     * @return {jQuery.Deferred} jQuery.Deferred object.
+     */
+    getCurrentPositionDeferred: function(options) {
+      var deferred = $.Deferred();
+      navigator.geolocation.getCurrentPosition(
+        deferred.resolve, deferred.reject, options);
+      return deferred.promise();
+    },
+    /**
+     * @param {PositionOptions} options HTML5 Geolocation properties.
+     * @return {Plugin} The Plugin instance.
+     */
+    watchPositionWithNotifying: function(options) {
+      return new Plugin(options);
+    }
+  }
+}(jQuery, this));
---
+++ grunt.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    growl: {
+      compileMessage: {
+        message: "Compiled!!",
+        title: "Grunt compile"
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'concat min growl:compileMessage'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  grunt.loadNpmTasks('grunt-growl');
+
+  // Default task.
+  grunt.registerTask('default', 'concat min growl:compileMessage');
+
+};
